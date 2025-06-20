ea24aa1bb7ca71663d9a5279c3ec570a1912f73b
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-06-20 19:03:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,8 +54,6 @@ from TechVJ.bot.clients import initialize_clients
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-StreamBot.start()
-loop = asyncio.get_event_loop()
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
