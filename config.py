3f9fc97bb85b6e672e23a7a228b7563796ec5993
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-09-08 19:28:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,7 +83,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://narrow-margaret-vktheencoder-5d3ed5b6.koyeb.app/")
+URL = environ.get("URL", "https://unemployed-vicki-vktheencoder-27930a31.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
