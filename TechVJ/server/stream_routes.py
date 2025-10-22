611460775e9cb4b4a19248d51de472c40e52b33e
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-10-22 21:45:19 +0530

    Update stream_routes.py

--- TechVJ/server/stream_routes.py
+++ TechVJ/server/stream_routes.py
@@ -155,14 +155,24 @@ async def media_streamer(request: web.Request, id: int, secure_hash: str):
             mime_type = "application/octet-stream"
             file_name = f"{secrets.token_hex(2)}.unknown"
 
-    return web.Response(
-        status=206 if range_header else 200,
-        body=body,
-        headers={
-            "Content-Type": f"{mime_type}",
-            "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Length": str(req_length),
-            "Content-Disposition": f'{disposition}; filename="{file_name}"',
-            "Accept-Ranges": "bytes",
-        },
+        resp = web.StreamResponse(
+            status=206 if range_header else 200,
+            headers={
+                "Content-Type": f"{mime_type}",
+                "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
+                "Content-Length": str(req_length),
+                "Content-Disposition": f'{disposition}; filename="{file_name}"',
+                "Accept-Ranges": "bytes",
+            },
+        )
+        await resp.prepare(request)
+        
+        try:
+            async for chunk in body:
+                await resp.write(chunk)
+            await resp.write_eof()
+        except (ConnectionResetError, BrokenPipeError, BadStatusLine):
+            logging.warning("Client disconnected during stream")
+            return resp
+        return resp
     )
