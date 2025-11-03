be954bc778c62dc50f556303dcf50d8c33eabb66
fackyhindi03 <fackyhindidonghua03@gmail.com>
2025-11-03 09:50:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,10 +20,10 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "27999679"))
 API_HASH = environ.get("API_HASH", "f553398ca957b9c92bcb672b05557038")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7947042930:AAG2PfxNbM_CKifeOrZgi_A44VIZ7sQ-7uc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7260809129:AAGiRnpJrr7OqMmZbUovi6wxGP5DF-LVKrg")
 
 PICS = (environ.get('PICS', 'https://static.wikia.nocookie.net/xian-ni/images/1/11/Wanglin.webp/revision/latest?cb=20241209233813')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1423807625 1048110820').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1423807625 1048110820 6520490787').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "file_sharing_bot03_bot") # without @
 PORT = environ.get("PORT", "8000")
 
